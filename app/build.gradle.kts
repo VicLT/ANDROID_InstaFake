07fb684a63d9d63b89001c6869c1ae5fa719f092
Victor LT <victor_lamas@hotmail.com>
2025-08-02 16:20:14 +0200

    Preparando la navegación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -51,7 +52,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    // Extras
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
 
     // Testing
     testImplementation(libs.junit)
---
+++ app/src/main/java/udemy/victorlamas/instafake/view/core/navigation/NavigationWrapper.kt
@@ -0,0 +1,17 @@
+package udemy.victorlamas.instafake.view.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import udemy.victorlamas.instafake.view.auth.login.LoginScreen
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Login) {
+         composable<Login> {
+             LoginScreen()
+         }
+    }
+}
