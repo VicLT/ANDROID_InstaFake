ae1bba9d75e21411d12fd0aa36d98ba369b3ab8f
VictorLT <victor_lamas@hotmail.com>
2025-08-29 16:40:45 +0200

    Añadir Dagger Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -51,6 +53,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Dependencies Injection
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     // Extras
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
---
+++ app/src/main/java/udemy/victorlamas/instafake/InstaFakeApp.kt
@@ -0,0 +1,7 @@
+package udemy.victorlamas.instafake
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InstaFakeApp: Application()
